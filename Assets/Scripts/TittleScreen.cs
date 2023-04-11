774b7f20a9d5a08ba8b27ec5fc1c342887fd2dcb
Arthur M. S. de Oliveira <66339390+ArthurMS15@users.noreply.github.com>
2023-04-11 16:04:51 -0300

    Update TittleScreen.cs
    Mensagem de certeza

--- Assets/Scripts/TittleScreen.cs
+++ Assets/Scripts/TittleScreen.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEditor;
 
 public class TittleScreen : MonoBehaviour
 {
@@ -10,6 +11,9 @@ public class TittleScreen : MonoBehaviour
     }
 
     public void QuitGame(){
-        Application.Quit();
+        bool playOutput = EditorUtility.DisplayDialog("Sair do jogo", "Você tem certeza que quer sair do jogo?", "Sim", "Não");
+        if (playOutput){
+            Application.Quit();
+        }
     }
 }
