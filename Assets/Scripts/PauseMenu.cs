eac8f3444c0315b092e55c37e0a6306ad689d465
Arthur M. S. de Oliveira <66339390+ArthurMS15@users.noreply.github.com>
2023-04-16 22:05:16 -0300

    Tela de pause

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public static bool isPaused;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(isPaused){
+                ResumeGame();
+            } else {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame(){
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+    public void ResumeGame(){
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+    public void GoToMainMenu(){
+        Time.timeScale = 1f;
+        bool playOutput = EditorUtility.DisplayDialog("Voltar ao título", "Você tem certeza que quer voltar para a tela de título", "Sim", "Não");
+        if (playOutput){
+            SceneManager.LoadScene ("Menu");
+        }
+        isPaused = false;
+    }
+    public void QuitGame(){
+        bool playOutput = EditorUtility.DisplayDialog("Sair do jogo", "Você tem certeza que quer sair do jogo?", "Sim", "Não");
+        if (playOutput){
+            Application.Quit();
+        }
+    }
+}
