13ac20d5e4f635c5887be5b0304cd43b02b3fff6
marlonvcendron <marlonvcendron@gmail.com>
2023-04-23 23:40:15 -0300

    m

--- Assets/Scripts/BoatController.cs
+++ Assets/Scripts/BoatController.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BoatController : MonoBehaviour {
     public BucketController bucketController;
     public float thrustForce = 10f;
     public float maxThrustTime = 1f;
     public float recoveryTime = 1f;
+    public Image staminaBarFill;
     public float stamina = 100f;
 
     private Rigidbody rb;
@@ -12,9 +14,11 @@ public class BoatController : MonoBehaviour {
     private float rightThrustTimer;
     private bool leftPaddleCooldown;
     private bool rightPaddleCooldown;
+    private float staminaRecoveryRate = 5f;
+    private float staminaDecreaseRate = 10f;
+    private float minStaminaToRow = 5f;
 
-    void Start()
-    {
+    void Start() {
         rb = GetComponent<Rigidbody>();
         leftThrustTimer = 0f;
         rightThrustTimer = 0f;
@@ -22,8 +26,7 @@ public class BoatController : MonoBehaviour {
         rightPaddleCooldown = false;
     }
 
-    void Update()
-    {
+    void Update() {
         CheckInput();
         UpdateTimers();
     }
@@ -33,23 +36,52 @@ public class BoatController : MonoBehaviour {
             return;
         }
 
-        if (Input.GetKey(KeyCode.A) && !leftPaddleCooldown) {
-            leftThrustTimer += Time.deltaTime;
-            if (leftThrustTimer <= maxThrustTime) {
-                ApplyThrust(-1);
-            } else {
-                leftPaddleCooldown = true;
+        bool rowing = false;
+
+        if (stamina >= minStaminaToRow) {
+            if (Input.GetKey(KeyCode.A) && !leftPaddleCooldown) {
+                rowing = true;
+                leftThrustTimer += Time.deltaTime;
+                if (leftThrustTimer <= maxThrustTime) {
+                    ApplyThrust(-1);
+                } else {
+                    leftPaddleCooldown = true;
+                }
+            }
+
+            if (Input.GetKey(KeyCode.D) && !rightPaddleCooldown) {
+                rowing = true;
+                rightThrustTimer += Time.deltaTime;
+                if (rightThrustTimer <= maxThrustTime) {
+                    ApplyThrust(1);
+                } else {
+                    rightPaddleCooldown = true;
+                }
             }
         }
 
-        if (Input.GetKey(KeyCode.D) && !rightPaddleCooldown) {
-            rightThrustTimer += Time.deltaTime;
-            if (rightThrustTimer <= maxThrustTime) {
-                ApplyThrust(1);
-            } else {
-                rightPaddleCooldown = true;
-            }
+        UpdateStamina(rowing);
+    }
+
+    private void UpdateStamina(bool rowing) {
+        if (rowing) {
+            stamina -= staminaDecreaseRate * Time.deltaTime;
+            stamina = Mathf.Clamp(stamina, 0, 100);
+        } else {
+            stamina += staminaRecoveryRate * Time.deltaTime;
+            stamina = Mathf.Clamp(stamina, 0, 100);
         }
+
+        UpdateStaminaBar();
+    }
+
+    private void UpdateStaminaBar() {
+        float staminaPercentage = stamina / 100f;
+
+        RectTransform rectTransform = staminaBarFill.GetComponent<RectTransform>();
+        Vector2 newSizeDelta = rectTransform.sizeDelta;
+        newSizeDelta.x = staminaPercentage * rectTransform.parent.GetComponent<RectTransform>().sizeDelta.x;
+        rectTransform.sizeDelta = newSizeDelta;
     }
 
     private void ApplyThrust(int direction) {
